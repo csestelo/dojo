f2f692f2218c444b6aa398402ebc4cdc96c24ab5
Cintia Sestelo <sestelos@gmail.com>
2016-03-23 01:08:25 -0300

    Finishing happy numbers exercise

--- happy_numbers/happy_numbers.py
+++ happy_numbers/happy_numbers.py
@@ -1,8 +1,37 @@
 def happy_number(number):
+    verification = []
     while number != 1:
         str_number = str(number)
         calculating_number = []
         for digit in str_number:
             calculating_number.append(int(digit)**2)
         number = sum(calculating_number)
+        if number in verification:
+                return False
+        verification.append(number)
     return number == 1
+
+
+def happy_number2(number):
+    verification = set()
+    while number != 1 and number not in verification:
+        verification.add(number)
+        str_number = str(number)
+        number = 0
+        for digit in str_number:
+            number += int(digit)**2
+    return number == 1
+
+
+def happy_number3(number):
+    def is_happy(num, verification):
+        if num == 1 or num in verification:
+            return num == 1
+        else:
+            verification.append(num)
+            str_number = str(num)
+            num = 0
+            for digit in str_number:
+                num += int(digit)**2
+            return is_happy(num, verification)
+    return is_happy(number, set())
--- happy_numbers/test_happy_numbers.py
+++ happy_numbers/test_happy_numbers.py
@@ -1,5 +1,5 @@
 import unittest
-from happy_numbers import happy_number
+from happy_numbers import happy_number2 as happy_number
 
 class TestHappyNumbers(unittest.TestCase):
     def test_number_1_must_return_true(self):
@@ -10,6 +10,12 @@ class TestHappyNumbers(unittest.TestCase):
 
     def test_number_13_must_return_true(self):
         self.assertTrue(happy_number(13))
+
+    def test_number_6_must_return_false(self):
+        self.assertFalse(happy_number(6))
+
+    def test_number_0_must_return_false(self):
+        self.assertFalse(happy_number(0))
 
 
 unittest.main()
