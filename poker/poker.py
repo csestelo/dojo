a3b731937a551d77e13ad4b9c621c34e10d5c3bc
Cintia Sestelo <sestelos@gmail.com>
2016-03-26 03:21:59 -0300

    Add poker exercise not finished

---
+++ poker/poker.py
@@ -0,0 +1,50 @@
+DECK = {'valete': 11, 'dama': 12, 'rei': 13, 'as': 14}
+#one_pair = 15 two_pair = 16 three_of_a_kind = 17 straight = 18
+#flush = 19 full_house = 20 four_of_a_kind = 21 straight_flush = 22
+#royal_flush = 23
+
+def poker(hand_1, hand_2):
+    if one_pair(hand_1) > one_pair(hand_2):
+        return 'Player_1'
+    else:
+        return 'Player_2'
+
+def normalize_value(hand):
+    final_hand = []
+    for card in hand:
+        if card in DECK:
+            final_hand.append(DECK[card])
+        else:
+            final_hand.append(card)
+    return final_hand
+
+def higher_card(hand):
+    return max(normalize_value(hand))
+
+def one_pair(hand):
+    check = []
+    for card in normalize_value(hand):
+        if card in check:
+            return 15
+        else:
+            check.append(card)
+    return higher_card(check)
+
+def one_pair(hand):
+    count = {}
+    for card in normalize_value(hand):
+        val = 0
+        if card in count:
+            count[card] = val + 1
+        else:
+#assim se cria uma nova chave/valor em um dicionario>
+            count[card] = 0
+    for checked_card in count.values():
+        if checked_card == 4:
+            return 21
+        elif checked_card == 3:
+            return 17
+        elif checked_card == 2:
+            return 15
+        else:
+            higher_card(hand)
---
+++ poker/test_poker.py
@@ -0,0 +1,33 @@
+import unittest
+from poker import poker, normalize_value
+
+DECK = {'valete': 11, 'dama': 12, 'rei': 13, 'as': 14}
+
+
+class TestPoker(unittest.TestCase):
+    def test_a_higher_number_card_must_win(self):
+        self.assertEqual(poker([4, 5, 7, 2, 8],
+                        [3, 2, 8, 5, 10]), 'Player_2')
+
+    def test_a_higher_card_in_hand_must_win(self):
+        self.assertEqual(poker([3, 5, 7, 9, 'dama'],
+                        [5, 3, 7, 4, 'valete']), 'Player_1')
+
+    def test_one_pair_must_win_higher_card(self):
+        self.assertEqual(poker([7, 5, 8, 8, 9],
+                        [2, 5, 7, 9, 'valete']), 'Player_1')
+
+    # def test_two_pair_must_win_op_and_hc(self):
+    #     self.assertEqual(poker([]))
+
+    def test_toak_must_win_one_pair(self):
+        self.assertEqual(poker([4, 5, 8, 8, 8],
+                        [3, 5, 10, 10, 'as']), Player_1)
+
+    def test_foak_must_win_toak(self):
+        self.assertEqual(poker([4, 8, 8, 8, 8],
+                        ['as', 5, 'as', 10, 'as']), Player_1)
+
+
+
+unittest.main()
